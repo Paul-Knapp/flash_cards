3e9de0e987051932ceba3f9f510872ae980319d2
Paul Knapp <ppankluap@gmail.com>
2024-08-22 09:44:12 -0500

    add comments to clarify operation

--- lib/card.rb
+++ lib/card.rb
@@ -1,8 +1,10 @@
 class Card
-    attr_reader :question, :answer, :category 
+    attr_reader :question, :answer, :category #an attribute reader to increase the scope of the variables
     def initialize(question , answer , category)
+        #create each card with a catagory, a question and an answer
         @question = question
         @answer = answer
         @category = category
+        #sets the instance variables to be equal to the variables in the attribute reader.
     end
 end
--- lib/turn.rb
+++ lib/turn.rb
@@ -1,2 +1,8 @@
 require './lib/turn'
 require './lib/card'
+
+class Turn 
+    def initialize(guess , Card)
+        
+    end
+end
