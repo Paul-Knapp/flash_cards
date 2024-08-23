a596284aea7ac734533928d2c850edee9cad66a8
Paul Knapp <ppankluap@gmail.com>
2024-08-22 20:44:59 -0500

    Add a test for guess functionality

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,8 +1,18 @@
 require './lib/turn'
+require './lib/card'
 
 RSpec.describe Turn do 
     it 'exists' do 
-        turn = Turn.new("Juneau", card )
+        turn = Turn.new("Juneau", Card )
+        #create an instance of the card class for testing
+        #require 'pry' ;Binding.pry
         expect(turn).to be_instance_of(Turn)
     end 
+
+    it "has a guess" do   
+        turn = Turn.new("Juneau", Card )
+
+        expect(turn.guess).to eq("Juneau")
+    end
+
 end
