c11fc9787461f39134df68b0902a4e34e90f7a62
Paul Knapp <ppankluap@gmail.com>
2024-08-24 11:48:08 -0500

    complete the deck class and test file

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,8 +1,22 @@
 class Deck
 
-    attr_reader :cards
+    attr_accessor :cards, :card
     
-    def initiallize([cards])
+    def initialize(*cards)
+        #the splat operator allows multiple arguements to be passed in
+        #   and creates an array with the arguements as the elements in it.
         @cards = cards
     end
+
+    def count
+        #create a method called .count
+        cards.length
+        #make the .count method query the length of the array
+    end
+
+    def cards_in_category(parameter)
+       @cards.find_all { |card| card.category == parameter }
+
+    end
+
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -37,7 +37,7 @@ RSpec.describe Deck do
         #check if the .count method = the length of the array
     end
 
-    xit "can sort cards by category" do
+    it "can sort cards by category" do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -45,7 +45,7 @@ RSpec.describe Deck do
 
         deck = Deck.new(card_1, card_2, card_3)
          #create an instance of the deck class
-         
-        expect(deck.cards_in_category(:STEM)).to eq(card_2, card_3)
+        
+        expect(deck.cards_in_category(:STEM)).to contain_exactly(card_2, card_3)
     end
 end
