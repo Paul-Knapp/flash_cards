5ee436af8563a3d1a3424d9c267706433012e126
Paul Knapp <ppankluap@gmail.com>
2024-08-27 18:57:30 -0500

    Fix formatting issue in the class

--- lib/card.rb
+++ lib/card.rb
@@ -1,16 +1,14 @@
-
-
 class Card
 
     attr_accessor :question, 
                   :answer, 
                   :category 
-    #an attribute reader to increase the scope of the variables
+    # an attribute reader to increase the scope of the variables
     def initialize(question , answer , category)   #create each card with a catagory, a question and an answer
         @question = question
         @answer = answer
         @category = category
-        #sets the instance variables to be equal to the a local variable in the attribute reader.
+        # sets the instance variables to be equal to the a local variable in the attribute reader.
     end
 
 end
