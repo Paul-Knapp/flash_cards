fffd1ed2ae9823f9ed65251d288a9b519a9d90df
Paul Knapp <ppankluap@gmail.com>
2024-08-27 18:58:56 -0500

    Remove extra space and increase readability

--- lib/round.rb
+++ lib/round.rb
@@ -1,11 +1,12 @@
 
 class Round
     attr_reader :deck, :turns, :card_number
-    #allow acces to the variables that will be queried
+    # allow acces to the variables that will be queried
     def initialize(deck)
         @deck = deck
         @turns = []
         @card_number = 0
+        card_number = @card_number
     end
     
     def current_card
@@ -14,29 +15,29 @@ class Round
 
     def take_turn(guess)
        new_turn = Turn.new(guess, current_card)
-       #create new instance of a turn for the guess to be used on 
+       # create new instance of a turn for the guess to be used on 
        @turns << new_turn
-       #puts the new turn into the turns array
+       # puts the new turn into the turns array
        @card_number += 1
        new_turn
     end
 
     def number_correct
         @turns.count { |turn| turn.correct? }
-        #this method looks at the turns array and counts the number of
-        #correct answers. it is almost shorthand for the .each
+        # this method looks at the turns array and counts the number of
+        # correct answers. it is almost shorthand for the .each
     end
 
     def number_correct_by_category(category)
         @turns.count { |turn| turn.correct? && turn.card.category == catagory}
-        #runs the .count method on the "each turn in the turns array"
-        #returns the number of turns that are correct and in the queried category
+        # runs the .count method on the "each turn in the turns array"
+        # returns the number of turns that are correct and in the queried category
     end
 
     def percent_correct
-        (number_correct.to_f / @turns.count * 100).round(1)
-        #does math to convert a number to a percent with a float value 
-        #the ruby language rounds integers when divided so the float returns
+        (number_correct.to_f / @turns.count * 100).round(0)
+        # does math to convert a number to a percent with a float value 
+        # the ruby language rounds integers when divided so the float returns
         # a more accurate result
     end
 
@@ -44,7 +45,7 @@ class Round
         category_turns = @turns.select { |turn| turn.card.category == category }
         correct_in_category = category_turns.count { |turn| turn.correct? }
         return 0.0 if category_turns.empty?
-        (correct_in_category.to_f / category_turns.count * 100).round(1)
+        (correct_in_category.to_f / category_turns.count * 100).round(0)
     end
 
 end 
