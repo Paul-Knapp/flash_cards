1fc5907833e5e47e31905c22779eb85ebff142ec
Paul Knapp <ppankluap@gmail.com>
2024-08-27 18:57:55 -0500

    Fix formatting and readability

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,16 +1,14 @@
 class Deck
-
     attr_reader :cards
-    
     def initialize(cards)
-        #   and creates an array with the arguements as the elements in it.
+        # instatiates the class and creates an array with the arguements as the elements in it.
         @cards = cards
     end
 
     def count
-        #create a method called .count
+        # create a method called .count
         cards.length
-        #make the .count method query the length of the array
+        # make the .count method query the length of the array
     end
 
     def cards_in_category(parameter)
